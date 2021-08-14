c5722daedd9010805ed57f775e75e59a7cdde3dc
Rumen Sinigerski <69121051+RumenSinigerski@users.noreply.github.com>
2021-08-14 23:23:01 +0300

    Scaffold Identity

---
+++ TankCleaningProject/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TankCleaningProject.Data;
+
+namespace TankCleaningProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext data;
+
+        public UserController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
